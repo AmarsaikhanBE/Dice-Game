b296921db246ae88745124d47a3734cb8a9b1b50
@dmin <81603165+AmarsaikhanBE@users.noreply.github.com>
2022-03-24 16:00:14 +0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let player1Name = localStorage.getItem('p1Name')
+let player2Name = localStorage.getItem('p2Name')
+if (player1Name == '') {
+    humanName.textContent = 'Player 1'
+} else {
+    humanName.textContent = player1Name
+}
+if (player2Name == '') {
+    computer.textContent = 'Player 2'
+} else {
+    computer.textContent = player2Name
+}
+let humanWin
+humanWin = Number(localStorage.getItem('humanWinNum'))
+let cpuWin
+cpuWin = Number(localStorage.getItem('cpuWinNum'))
+humanWinCounter.textContent = humanWin
+cpuWinCounter.textContent = cpuWin
 const dice = ['images/dice-1.png', 'images/dice-2.png', 'images/dice-3.png', 'images/dice-4.png', 'images/dice-5.png', 'images/dice-6.png']
 const diceImg = document.querySelector('img.dice')
 const clickButton = (x) => {
@@ -9,11 +27,15 @@ const saveScore = () => {
     localStorage.setItem('round', round)
     localStorage.setItem('playerScore', humanScore.textContent)
     localStorage.setItem('cpuScore', cpuScore.textContent)
+    localStorage.setItem('humanWinNum', humanWin)
+    localStorage.setItem('cpuWinNum', cpuWin)
+    localStorage.setItem('p1Name', player1Name)
+    localStorage.setItem('p2Name', player2Name)
 }
 humanScore.textContent = localStorage.getItem('playerScore')
 cpuScore.textContent = localStorage.getItem('cpuScore')
 let round = localStorage.getItem('round')
-if (round == 'player') {
+if (round == 'cpu') {
     humanName.style.fontWeight = '900'
     computer.style.fontWeight = '100'
 } else {
@@ -34,8 +56,10 @@ playDice.addEventListener('click', () => {
         humanName.style.fontWeight = '900'
         computer.style.fontWeight = '100'
         if (Number(humanScore.textContent) >= 100) {
-            alert('You win!')
+            alert('Winner is: Player 1')
             restartGame()
+            humanWin++
+            humanWinCounter.textContent = humanWin
         }
     } else {
         clickButton(cpuScore)
@@ -43,12 +67,30 @@ playDice.addEventListener('click', () => {
         humanName.style.fontWeight = '100'
         computer.style.fontWeight = '900'
         if (Number(cpuScore.textContent) >= 100) {
-            alert('You lose!')
+            alert('Winner is: Player 2')
             restartGame()
+            cpuWin++
+            cpuWinCounter.textContent = cpuWin
         }
     }
     saveScore()
 })
 newGame.addEventListener('click', () => {
+    humanWin = 0
+    cpuWin = 0
+    humanWinCounter.textContent = humanWin
+    cpuWinCounter.textContent = cpuWin
+    player1Name = prompt("Player 1")
+    player2Name = prompt('Player 2')
+    if (player1Name == '') {
+        humanName.textContent = 'Player 1'
+    } else {
+        humanName.textContent = player1Name
+    }
+    if (player2Name == '') {
+        computer.textContent = 'Player 2'
+    } else {
+        computer.textContent = player2Name
+    }
     restartGame()
 })
